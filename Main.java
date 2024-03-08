c564208ff6169d7d46161ff031f12a7170afb636
Nishchal <nischal.15gupta@gmail.com>
2024-03-08 12:50:08 +0530

    nForest2 pattern problem solution

--- Main.java
+++ Main.java
@@ -1,42 +1,12 @@
 /*
-Problem statement
-Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
+Problem: https://www.codingninjas.com/studio/problems/n-2-forest_6570178
 
-For every value of ‘N’, help sam to print the corresponding N-dimensional forest.
-
-Example:
-Input: ‘N’ = 3
-
-Output: 
+* 
+* *
 * * *
-* * *
-* * *
-
-Constraints :
-1  <= N <= 25
-Time Limit: 1 sec
-Sample Input 1:
-3
-Sample Output 1:
-* * *
-* * *
-* * *
-Explanation Of Sample Input 1 :
-For N = 3, fill all the rows and columns in 3x3 matrix with ‘*’.
-Sample Input 2 :
-1
-Sample Output 2 :
-*
-Sample Input 3 :
-4
-Sample Output 3 :
-* * * *
-* * * *
-* * * *
 * * * *
 
  */
-
 import java.util.Scanner;
 public class Main {
 
@@ -45,19 +15,21 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         //call the nForest function and pass the integer input taken from the user i.e. n
-        nForest(n);
+        nForest2(n);
     }
 
-    public static void nForest(int n){
-    int stars = 3, spaces = 0;
+    public static void nForest2(int n){
+    int stars = 1;
     for(int i = 1; i <= n; ++i){
         for(int j = 1; j <= stars; ++j){
+            //print the stars in the current row
             System.out.print('*');
-            //print space between each star
+            //print the space
             System.out.print(" ");
         }
-        //print a new line
+        //print a new line after the end of each row
         System.out.println();
+        stars++;
     }
 }
 }
